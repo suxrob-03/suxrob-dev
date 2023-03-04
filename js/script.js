7139e2625065322e3eb16fc3b9f03b2b53a98c2c
suxrob-03 <suxrobr266@gmail.com>
2023-03-04 11:58:58 +0500

    I have a personal site

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// :nth-of-type(2)
+
+$(function () {
+    // alert('salom')
+    $("nav .menu i:nth-of-type(1)").click(function () {
+        $(".menu_ul").toggle("fast");
+        $("nav .menu i:nth-of-type(1)").css("display" ,"none");
+        $("nav .menu i:nth-of-type(2)").css("display" ,"block");
+    });
+    $("nav .menu i:nth-of-type(2)").click(function () {
+        $(".menu_ul").toggle("fast");
+        $("nav .menu i:nth-of-type(1)").css("display" ,"block");
+        $("nav .menu i:nth-of-type(2)").css("display" ,"none");
+    });
+
+
+    $("nav .menu .menu_ul ul li").click(function(){
+        $(".menu_ul").css("display" ,"none");
+    });
+    
+});
+
+// display: none;
